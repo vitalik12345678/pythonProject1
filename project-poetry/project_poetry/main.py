5ecb19825a7159206f1dfbc82c5a94559ec0a892
vitalik <vitalik142003@gmail.com>
2021-09-30 21:32:53 +0300

    Initial commit

---
+++ project-poetry/project_poetry/main.py
@@ -0,0 +1,11 @@
+__version__ = '0.1.0'
+
+from flask import Flask
+app =Flask(__name__)
+
+@app.route('/api/v1/hello-world/<name>')
+def user(name):
+    return '<h1>Hello  %s!</h1>' % name
+
+if __name__ == '__main__':
+    app.run(debug=True)
