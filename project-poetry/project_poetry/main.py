96e6ecc647d9a6821c2e16f0cff6c2475c673daf
vitalik <vitalik142003@gmail.com>
2021-09-30 21:46:14 +0300

    Add contorller

--- project-poetry/project_poetry/main.py
+++ project-poetry/project_poetry/main.py
@@ -5,7 +5,7 @@ app =Flask(__name__)
 
 @app.route('/api/v1/hello-world/<name>')
 def user(name):
-    return '<h1>Hello  %s!</h1>' % name
+    return '<h1>Hello World  %s!</h1>' % name
 
 if __name__ == '__main__':
     app.run(debug=True)
